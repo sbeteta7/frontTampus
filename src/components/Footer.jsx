e70ee5389a6555964ce3c43e692d3c81b62357ee
MarceloxASG <jeffingaleus254@gmail.com>
2023-11-30 01:03:16 -0500

    tampus

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                     </svg>
-                    <span class="ml-3 text-xl">Tailblocks</span>
+                    <span class="ml-3 text-xl">Tampus</span>
                 </a>
-                <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+                <p class="mt-2 text-sm text-gray-500">Comodidad y seguridad a la distancia perfecta...</p>
                 </div>
                 <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
                 <div class="lg:w-1/4 md:w-1/2 w-full px-4">
@@ -84,8 +84,8 @@ const Footer = () => {
             </div>
             <div class="bg-gray-100">
                 <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-                    <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+                <p class="text-gray-500 text-sm text-center sm:text-left">© 2023 Tampus —
+                    <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@penguinsoncalaminas</a>
                 </p>
                 <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
                     <a class="text-gray-500">
