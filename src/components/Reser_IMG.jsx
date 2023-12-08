b329f21d3aa9a42f1e7f930940af81adc4b6a683
MarceloxASG <jeffingaleus254@gmail.com>
2023-12-08 09:43:40 -0500

    images reserv

---
+++ src/components/Reser_IMG.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Reser_IMG() {
+  return (
+    <div className='max-w-[1140px] m-auto w-full px-4 py-16'>
+        <div className='grid sm:grid-cols-5 gap-4'>
+            <div className='sm:col-span-3 col-span-2 row-span-2'>
+                <img className='w-full h-full object-cover' src="https://picsum.photos/800/600" alt="/" />
+            </div>
+            <div>
+                <img className='w-full h-full object-cover' src="https://picsum.photos/800/600" alt="/" />
+            </div>
+            <div>
+                <img className='w-full h-full object-cover' src="https://picsum.photos/800/600" alt="/" />
+            </div>
+            <div>
+                <img className='w-full h-full object-cover' src="https://picsum.photos/800/600" alt="/" />
+            </div>
+            <div>
+                <img className='w-full h-full object-cover' src="https://picsum.photos/800/600" alt="/" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Reser_IMG
